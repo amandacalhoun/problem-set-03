07ff5ac8543c91a8ca1d72cbdbd69db655e060a8
Amanda Calhoun <amandacalhoun15@gmail.com>
2025-02-23 19:30:31 -0500

    Submission Files

--- AC_PSET3_Code.m
+++ AC_PSET3_Code.m
@@ -1,27 +1,28 @@
-% PS99_DMG.M in this problem set I demonstrate how the universe can be simulated
-% with just three lines of MatLab code.
-% SYNTAX:   [U,W,I]=ps99_dmg(X,Y,alpha)
+% PS03_DMG.M in this problem set I demonstrate how to use PCA/FA on 
+%a real environmental datset
+%SYNTAX
+%   [U sumry AR SR]=PCA2(DAT,NFLAG) is an m-function that provides eigenvectors, 
+%	eigenvalues, their % total variance, their cumulative % variance, principle 
+%	component loadings, and principle component scores of the data provided 
+%	in the matrix DAT.
+% INPUT:
+%	DAT is an n x m data matrix
+%	NFLAG = 0, do not standardize data
+%	      = 1, standardize data
+% OUTPUT:
+%	U is the m x m eigenvector matrix
+%	sumry is the m x 3 matrix containing eigenvalues, percent variance, and
+%	      cumulative variance by column
+%	AR are the factor loadings for each variable (m x m)
+%	SR is the n x m matrix of factor scores by column
 %
-% INPUT:    X = a matrix containing the distribution of religions on earth
-%           Y = a vector containing the names of known star systems
-%           alpha = an insanity parameter that varies from zero to one
-% OUTPUT:   U = the universe matrix, distribution of carfloats
-%           W = the world matrix, distribution of ring bearers
-%           I = index vector containing the indices of ICBM's
-%
-% Started 2028:09:11 D. Glover, WHOI
-% Modif'd 2028:09:12 DMG added insanity parameter to increase stability
-% Modif'd 2028:09:13 DMG fixed disp command in my final run before I
-% submitted my answer to the instructors.
-
-
+% Submitted 2025:02:23 A. Calhoun, Harvard
 
 %Clear previous work
 clc; clear; close all
 %load data
 load satl.dat
 depth = -satl(:,3);
-
 
 %Part A
 %Visualize data with four subplots
@@ -65,7 +66,7 @@ Xlwr = satl(satl(:,3)>1000,4:10);
 %suspect that there are at least two underlying factors that are responsible for the distribution of these variables: biogeochemical processes 
 % (photosynthesis, respiration, etc.) and physical processes (precipitation, upwelling, etc.). Let's see if you can design a principal 
 % component--factor analysis to elucidate these factors.
-%%%%ANSWER THIS PLEASE
+disp('Question 1:')
 fprintf("The variables longitude, latitude, and depth should not be included\n" + ...
     "in the current PCA/FA analyses. The latitude and longitude data will be\n" + ...
     "utilized later to determine where the factors are. Depth will not be utilized\n" + ...
@@ -73,7 +74,7 @@ fprintf("The variables longitude, latitude, and depth should not be included\n" 
     "in the data set among and between variables. Depth is collected at regular intervals\n" + ...
     "and does not demonstrate any other variation. We also know that depth is a primary control on\n" + ...
     "all of the parameters that will be included in the PCA, which is why we separate the data\n" + ...
-    " into groups above and below 1000 m depth. If we included depth in the PCA, we would learn\n" + ...
+    "into groups above and below 1000 m depth. If we included depth in the PCA, we would learn\n" + ...
     "that all variables have a relationship with depth, but we are tryign to distinguish other processes\n" + ...
     "that may be affecting the distribution of nutrients, temperature, and salinity, so we exclude depth.\n" + ...
     "when attempting to identify factors (ex. biogeochemical or physical) that are responsible for the\n" + ...
@@ -81,11 +82,12 @@ fprintf("The variables longitude, latitude, and depth should not be included\n" 
 
 % %Discuss whether or not you think the data should be standardized; this 
 % % decision will effect the results of the rest of the problem, so think about it.
+disp('Question 2:')
 fprintf("The data should be standardized since the measured variables are on different scales and\n" + ...
     "have no defined upper limit. Therefore, it is best to standardize data to preserve the relative\n" + ...
     "differences in variables down depth profiles. Normalization (mean centering) would not be sufficient,\n" + ...
     "so we center around the column mean and scale variables by their standard deviations to put all variables\n" + ...
-    "on the same unitless scale.\n\n")
+    "on the same unitless scale. Otherwise differences in units or scale of variables would skew the results of the PCA.\n\n")
 
 
 
@@ -130,8 +132,8 @@ disp(ARlwr)
 
 %Part C
 %Calculate communalities of the factors by summing 
-Arupp = ARupp(:,1:3);
-Arlwr = ARlwr(:,1:3);
+Arupp = ARupp(:,1:4);
+Arlwr = ARlwr(:,1:4);
 for i = 1:size(Arlwr,1)   % Iterate over rows
     commlwr(i,1) = sum(Arlwr(i,:).^2);  % Sum of squares for each row
 end
@@ -141,12 +143,18 @@ for i = 1:size(Arupp,1)   % Iterate over rows
 end
 
 Rhat = Arupp*Arupp';
-comm3 = sqrt(diag(Rhat));
-
+comm3 = diag(Rhat);
+disp('Communalities Above 1000 m')
+disp(commlwr)
+disp('Communalities Below 1000 m')
+disp(commupp)
 
 % %What do these values represent?
+disp('Question 3:')
 fprintf("The communlatities represent how well the retained factors represent the\n" + ...
-    "total variance when compared to the original variance. In other words...")
+    "total variance of the original data vectors. In other words they indicate how well\n" + ...
+    "the factors that the model user chooses to keep can explain the variance of each data column\n" + ...
+    "in this example, aiding in the selection of the appropriate number of factors in factor analysis.\n\n")
 
 
 
@@ -156,70 +164,96 @@ fprintf("The communlatities represent how well the retained factors represent th
 % % they account for? What are their communalities?
 % 
 figure(2)
-h1 = plot(sumryupp (:,1), 'b-'); % Data points
+h1 = plot(sumryupp (:,2), 'b-'); % Data points
 hold on 
-h2 = plot(sumrylwr (:,1), 'r-'); % Data points
-xlabel('# Of Eigenvalue')
-ylabel('Eigenvalue')
+h2 = plot(sumrylwr (:,2), 'r-'); % Data points
+xlabel('PC')
+ylabel('Percent Variance Explained')
 set(gca, 'FontSize', 18, 'LineWidth', 2) 
 grid on
 title('Scree Plot PCA')
 legend([h1, h2], {'Above 1000 m', 'Below 100 m'}, 'Location', 'best');
 
+%Loadings, magnitudes of eigenvalues
+disp('Question 4:')
+fprintf("From the PCA/FA analysis, I believe that 3 or 4 factors should be kept for the data\n" + ...
+    "above and below 1000 m depth, depending on the real-world implications of the data analysis.\n" + ...
+    "If using this PCA/FA for a paper, I would include four factors and describe how some variables seem\n" + ...
+    "to have independent behaviors that make inclusion of a fourth factor beneficial for explained variance.\n" + ...
+    "The loadings of the different factors, as visualized in the output images, suggest that three\n" + ...
+    "factors are likely sufficient to describe the variability in the data for both the shallow and deep\n" + ...
+    "ocean data. Similarly, the eigenvalues of the factors suggest that three factors are sufficient, since\n" + ...
+    "the third factors have eigenvalues of 0.89 and 1.00 for the shallow and deep data, respectively. The\n" + ...
+    "eigenvalues of the fourth factors are 0.43 and 0.24, respectively, which are significantly below a common\n" + ...
+    "benchmark of 1.0 as a cutoff for retaining factors. \n\nHowever, the communalities indicate that a fourth factor\n" + ...
+    "may be included if we hope to maximize explanation of variance of all variables. Three factors explain 92%% and\n" + ...
+    "96%% of the variance of the Xupp and Xlwr data, while four factors account for 98.3%% and 99.6%% of the variance of\n" + ...
+    "the Xupp and Xlwr data. Four factors explain almost all of the variance in the original datasets, while three explain\n" + ...
+    "over 90%% of the total variance, which may be sufficient for some analyses. The comparison of the communalities from\n" + ...
+    "FA using 3 vs. 4 factors suggest that four factors may be better for more comprehensive interpretation.\n" + ...
+    "The salinity in the Xupp dataset has a communality of 85%% with 3 factors,\n" + ...
+    "which increases to 99.8%% after addition of the fourth factor. Therefore, if we care about interpreting the\n" + ...
+    "behavior of this variable, we should retain four factors to be thorough, though three factors would likely\n" + ...
+    "suffice for some purposes. Similarly, in the Xlwr data, the communaltities of the salinity and silica\n" + ...
+    "are 88%% and 68%%, respectively. Upon addition of a fourth factor, these communalities increase to 95.8%% and 99.2%%, respectively.\n" + ...
+    "Therefore, there is a stronger argument for adding the fourth factor to the analysis of the deeper data to capture the\n" + ...
+    "behavior of silica in the deep ocean.")
 
-fprintf("Depending on the analysis, 3 or 4 factors should be kept for Xupp and Xlwr\n" + ...
-    "Thre factors account for ___ of the variance of the Xupp and ___ of the variance\n" + ...
-    "of Xlwr data. This shows that these three factors explain a large proportion of\n" + ...
-    "the variance in the original data. ")
+%Above 1000 m depth
+figure(3); clf 
+subplot(1,2,1)
+plot(1:7, 100*sumryupp(:,2), '-k')
+set(gca, 'XLim', [1 7], 'XTick', 1:1:7, 'YLim', [0 100])
+xlabel('PC'); ylabel('%')
+title('% of Variance by Each PC')
 
-% 
-% 
-% 
-% 
-% 
-% 
-% 
-% 
-% 
-% 
-% 
-% 
-% 
-% 
-% % >>load demo1.dat (return)
-% % >>load demo2.dat (return)
-% % >>load demo2.dat (return)
-% % >>demo1 (return)
-% % >>demo(1,1) (return)
-% % >>plot(demo1,demo2) (return)
-% % >> title('Demoplot')
-% % >> xlabel('demo1')
-% % >> ylabel('demo2')
-% % >> text(10,100,'here is some text')
-% % a=('The information in the plot clearly shows the blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah...')
-% % a
-% % 
-% % % This m-file is part of the problem set demo created by T. Kenna on 8/16/98
-% % % note that lines that begin with % are ignored as program lines by matlab
-% % %
-% % % Here comes the real program
-% % %
-% % load demo1.dat % loads the first demo file
-% % load demo2.dat % loads the second demo file
-% % demo1(1,1) % prints the first member of demo1
-% % figure(1)
-% % plot(demo1,demo2)
-% % title('Demoplot')
-% % xlabel('demo1')
-% % ylabel('demo2')
-% % text(10,100,'here is some text')
-% % a=('The information in the plot clearly shows the blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah...')
-% % 
-% % disp('hit any key to resume program' )
-% %         pause % This will suspend the program until you are ready to move on
-% %         figure(2)
-% %         plot(sin(demo1),tan(demo2),'*g')
-% %         title('The second plot')
-% %         xlabel('Sine of demo1')
-% %         ylabel('Tangent of demo2')
-% %         b=('Plot number 2 shows the extent to which plot number 2 was yada yada yada')
+subplot(1,2,2)
+plot(1:7, cumsum(100*sumryupp(:,2)), '-k')
+set(gca, 'XLim', [1 7], 'XTick', 1:1:7, 'YLim', [0 100])
+xlabel('PC'); ylabel('%')
+title('Cumulative % of Variance')
+sgtitle('Scree Plot Above 1000 m', 'FontWeight', 'bold', 'FontSize', 18)
+
+figure(4); clf 
+plot(ARupp(:,1), '-ko', 'MarkerSize', 12); hold on 
+plot(ARupp(:,2), '-k^', 'MarkerSize', 12); hold on 
+plot(ARupp(:,3), '-kd', 'MarkerSize', 12); hold on
+plot(ARupp(:,4), '-ks', 'MarkerSize', 12); hold on
+plot(ARupp(:,5), '-k.', 'MarkerSize', 12); hold on
+plot(ARupp(:,6), '-kx', 'MarkerSize', 12); hold on
+plot(ARupp(:,7), '-kh', 'MarkerSize', 12);
+ylabel('Factor loadings of each PC')
+xlabeltext = {'Temp.'; 'Salinity'; 'Oxygen'; 'Phosphate'; 'Nitrate'; 'Nitrite'; 'Silica'};
+set(gca, 'xtick', 1:7, 'xticklabel', xlabeltext)
+legend('PC1', 'PC2', 'PC3', 'PC4', 'PC5', 'PC6', 'PC7')
+title('Factor Loadings for each PC Above 1000 m')
+
+
+%Below 1000 m depth
+figure(5); clf 
+subplot(1,2,1)
+plot(1:7, 100*sumrylwr(:,2), '-k')
+set(gca, 'XLim', [1 7], 'XTick', 1:1:7, 'YLim', [0 100])
+xlabel('PC'); ylabel('%')
+title('% of Variance by Each PC')
+
+subplot(1,2,2)
+plot(1:7, cumsum(100*sumrylwr(:,2)), '-k')
+set(gca, 'XLim', [1 7], 'XTick', 1:1:7, 'YLim', [0 100])
+xlabel('PC'); ylabel('%')
+title('Cumulative % of Variance')
+sgtitle('Scree Plot Below 1000 m', 'FontWeight', 'bold', 'FontSize', 18)
+
+figure(6); clf 
+plot(ARlwr(:,1), '-ko', 'MarkerSize', 12); hold on 
+plot(ARlwr(:,2), '-k^', 'MarkerSize', 12); hold on 
+plot(ARlwr(:,3), '-kd', 'MarkerSize', 12); hold on
+plot(ARlwr(:,4), '-ks', 'MarkerSize', 12); hold on
+plot(ARlwr(:,5), '-k.', 'MarkerSize', 12); hold on
+plot(ARlwr(:,6), '-kx', 'MarkerSize', 12); hold on
+plot(ARlwr(:,7), '-kh', 'MarkerSize', 12);
+ylabel('Factor loadings of each PC')
+xlabeltext = {'Temp.'; 'Salinity'; 'Oxygen'; 'Phosphate'; 'Nitrate'; 'Nitrite'; 'Silica'};
+set(gca, 'xtick', 1:7, 'xticklabel', xlabeltext)
+legend('PC1', 'PC2', 'PC3', 'PC4', 'PC5', 'PC6', 'PC7')
+title('Factor Loadings for each PC Below 1000 m')
